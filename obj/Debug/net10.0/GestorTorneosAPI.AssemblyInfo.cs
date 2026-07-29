03e7acca96dd7ccd79ca6a18e723afed9c553974
Yanel <Yanelgonzalez@gmail.com>
2026-07-29 18:36:27 -0400

    feat: completado el CRUD completo de Equipos en el controlador

--- obj/Debug/net10.0/GestorTorneosAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/GestorTorneosAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorTorneosAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f43f152993eb9d02842cc3caf419420f2b890f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a31cc1b575ca0f2e9e9a24a916d6c8eac0c77514")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorTorneosAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorTorneosAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
